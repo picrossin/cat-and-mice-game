ed1ecf402e46a317446952930b9760da8a7cf86a
picrossin <jadengoter@gmail.com>
2019-11-09 10:41:22 -0600

    Added throwing and holding

---
+++ Cat-and-Mice Game/Assets/Scripts/Interactable.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour, IInteractable
+{
+    public enum FoodType
+    {
+        Cheese
+    }
+
+    public FoodType m_FoodType;
+    public bool m_Held = false;
+
+    private Rigidbody m_ThisRigidbody = null;
+    private FixedJoint m_HoldJoint = null;
+
+
+    private void Start()
+    {
+        gameObject.tag = "Interactable";
+        m_ThisRigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        // If the holding joint has broken, drop the object
+        if (m_HoldJoint == null && m_Held == true)
+        {
+            m_Held = false;
+            m_ThisRigidbody.useGravity = true;
+        }
+    }
+
+    // Pick up the object, or drop it if it is already being held
+    public void Interact(UnityStandardAssets.Characters.FirstPerson.RigidbodyFirstPersonController playerScript)
+    {
+        Debug.Log("Pickup/drop");
+        // Is the object currently being held?
+        if (m_Held)
+        {
+            Drop();
+        }
+        else
+        {
+            Debug.Log("Picked up object");
+            m_Held = true;
+            m_ThisRigidbody.useGravity = false;
+
+            m_HoldJoint = playerScript.m_HandTransform.gameObject.AddComponent<FixedJoint>();
+            m_HoldJoint.breakForce = 10000f; // Play with this value
+            m_HoldJoint.connectedBody = m_ThisRigidbody;
+
+        }
+    }
+
+    // Throw the object
+    public void Action(UnityStandardAssets.Characters.FirstPerson.RigidbodyFirstPersonController playerScript)
+    {
+        // Is the object currently being held?
+        if (m_Held)
+        {
+            Drop();
+            Debug.Log("Attempting throw");
+            Vector3 forceDir = transform.position - playerScript.m_HandTransform.position;
+            m_ThisRigidbody.AddForce(forceDir * playerScript.movementSettings.m_ThrowForce);
+        }
+    }
+
+    // Drop the object
+    private void Drop()
+    {
+        m_Held = false;
+        m_ThisRigidbody.useGravity = true;
+
+        m_HoldJoint.connectedBody = null;
+    }
+}
