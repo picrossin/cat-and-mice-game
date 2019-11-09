8a4ef315ad6f2ec7863f8e34db38b13e2bbc8f68
picrossin <jadengoter@gmail.com>
2019-11-09 15:10:14 -0600

    More level stuff and some controller support

---
+++ Cat-and-Mice Game/Assets/Scripts/Mousehole.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Mousehole : MonoBehaviour
+{
+    public GameController m_GameController;
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Interactable")
+        {
+            FoodType item = collision.gameObject.GetComponent<Interactable>().m_FoodType;
+            if (m_GameController.m_ShoppingList.Any(i => i == item))
+            {
+                m_GameController.m_Collected.Add(item);
+            }
+            Destroy(collision.gameObject);
+        }
+    }
+}
