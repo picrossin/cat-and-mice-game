5ff0f97b7a8a272e75379e78b0ce3388794050d8
picrossin <jadengoter@gmail.com>
2019-11-09 23:36:09 -0600

    Made progress on level

---
+++ Cat-and-Mice Game/Assets/Scripts/Attack.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public int m_PlayerNumber = 3;
+    private bool inRange = false;
+    private bool canAttack = true;
+    private Collider mouseCollision;
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        Debug.Log("Inside range of attack");
+        mouseCollision = collision;
+        inRange = true;
+    }
+    private void OnTriggerExit(Collider collision)
+    {
+        Debug.Log("Leaving range of attack");
+        inRange = false;
+    }
+
+    public void Init(int playerNumber)
+    {
+        m_PlayerNumber = playerNumber;
+    }
+
+    void Update()
+    {
+        if (inRange)
+        {
+            if (Input.GetButtonDown("Hold" + m_PlayerNumber) && canAttack)
+            {
+                Debug.Log("Attempting attack");
+                if (mouseCollision.gameObject.tag == "Mouse")
+                {
+                    Debug.Log("Attack landed");
+                    UnityStandardAssets.Characters.FirstPerson.MouseController mouse =
+                        mouseCollision.gameObject.GetComponent<UnityStandardAssets.Characters.FirstPerson.MouseController>();
+                    mouse.playerHealth--;
+                    canAttack = false;
+                }
+            }
+            if (Input.GetButtonUp("Hold" + m_PlayerNumber))
+            {
+                canAttack = true;
+            }
+        }
+        
+    }
+}
--- Cat-and-Mice Game/Assets/Scripts/MouseController.cs
+++ Cat-and-Mice Game/Assets/Scripts/MouseController.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityStandardAssets.CrossPlatformInput;
 
 namespace UnityStandardAssets.Characters.FirstPerson
@@ -81,10 +82,12 @@ namespace UnityStandardAssets.Characters.FirstPerson
 
         public Camera cam;
         public int playerNumber = 1;
+        public int playerHealth = 3;
         public Transform m_HandTransform = null;
         public MovementSettings movementSettings = new MovementSettings();
         public MouseLook mouseLook = new MouseLook();
         public AdvancedSettings advancedSettings = new AdvancedSettings();
+        public CatController cat;
 
 
         private Rigidbody m_RigidBody;
@@ -93,7 +96,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
         private Vector3 m_GroundContactNormal;
         private bool m_Jump, m_PreviouslyGrounded, m_Jumping, m_IsGrounded, m_IsClimbing, m_PreviouslyClimbing, m_CanGrab;
         private Rigidbody m_HoldingItem;
-
+        private bool dead = false;
 
         public Vector3 Velocity
         {
@@ -139,87 +142,113 @@ namespace UnityStandardAssets.Characters.FirstPerson
             {
                 m_Jump = true;
             }
+
+            if (playerHealth == 0)
+            {
+                dead = true;
+
+                m_RigidBody.useGravity = false;
+                m_RigidBody.isKinematic = false;
+                Vector3 movePosition = transform.position;
+ 
+                movePosition.x = Mathf.MoveTowards(transform.position.x, cat.transform.position.x, 3f * Time.deltaTime);
+                movePosition.y = Mathf.MoveTowards(transform.position.y, cat.transform.position.y, 3f * Time.deltaTime);
+                movePosition.z = Mathf.MoveTowards(transform.position.z, cat.transform.position.z, 3f * Time.deltaTime);
+
+                m_RigidBody.MovePosition(movePosition);
+            }
         }
 
 
         private void FixedUpdate()
         {
-            GroundCheck();
-            Vector2 input = GetInput();
-
-            if ((Mathf.Abs(input.x) > float.Epsilon || Mathf.Abs(input.y) > float.Epsilon) && (advancedSettings.airControl || m_IsGrounded))
+            if (!dead)
             {
-                // always move along the camera forward as it is the direction that it being aimed at
-                Vector3 desiredMove = cam.transform.forward * input.y + cam.transform.right * input.x;
-                desiredMove = Vector3.ProjectOnPlane(desiredMove, m_GroundContactNormal).normalized;
+                GroundCheck();
+                Vector2 input = GetInput();
 
-                WallCheck(desiredMove);
-
-                desiredMove.x = desiredMove.x * movementSettings.CurrentTargetSpeed;
-                desiredMove.z = desiredMove.z * movementSettings.CurrentTargetSpeed;
-                desiredMove.y = desiredMove.y * movementSettings.CurrentTargetSpeed;
-
-                if (m_IsClimbing)
+                if ((Mathf.Abs(input.x) > float.Epsilon || Mathf.Abs(input.y) > float.Epsilon) && (advancedSettings.airControl || m_IsGrounded))
                 {
-                    m_RigidBody.velocity = Vector3.up * movementSettings.ClimbSpeed;
+                    // always move along the camera forward as it is the direction that it being aimed at
+                    Vector3 desiredMove = cam.transform.forward * input.y + cam.transform.right * input.x;
+                    desiredMove = Vector3.ProjectOnPlane(desiredMove, m_GroundContactNormal).normalized;
+
+                    WallCheck(desiredMove);
+
+                    desiredMove.x = desiredMove.x * movementSettings.CurrentTargetSpeed;
+                    desiredMove.z = desiredMove.z * movementSettings.CurrentTargetSpeed;
+                    desiredMove.y = desiredMove.y * movementSettings.CurrentTargetSpeed;
+
+                    if (m_IsClimbing)
+                    {
+                        m_RigidBody.velocity = Vector3.up * movementSettings.ClimbSpeed;
+                    }
+                    else
+                    {
+                        if (m_RigidBody.velocity.sqrMagnitude <
+                            (movementSettings.CurrentTargetSpeed * movementSettings.CurrentTargetSpeed))
+                        {
+                            m_RigidBody.AddForce(desiredMove * SlopeMultiplier(), ForceMode.Impulse);
+                        }
+                    }
+                }
+
+                if (m_IsGrounded)
+                {
+                    m_RigidBody.drag = 5f;
+
+                    if (m_Jump)
+                    {
+                        m_RigidBody.drag = 0f;
+                        m_RigidBody.velocity = new Vector3(m_RigidBody.velocity.x, 0f, m_RigidBody.velocity.z);
+                        m_RigidBody.AddForce(new Vector3(0f, movementSettings.JumpForce, 0f), ForceMode.Impulse);
+                        m_Jumping = true;
+                    }
+
+                    if (!m_Jumping && Mathf.Abs(input.x) < float.Epsilon && Mathf.Abs(input.y) < float.Epsilon && m_RigidBody.velocity.magnitude < 1f)
+                    {
+                        m_RigidBody.Sleep();
+                    }
                 }
                 else
                 {
-                    if (m_RigidBody.velocity.sqrMagnitude <
-                        (movementSettings.CurrentTargetSpeed * movementSettings.CurrentTargetSpeed))
-                    {
-                        m_RigidBody.AddForce(desiredMove * SlopeMultiplier(), ForceMode.Impulse);
-                    }
-                }
-            }
-
-            if (m_IsGrounded)
-            {
-                m_RigidBody.drag = 5f;
-
-                if (m_Jump)
-                {
                     m_RigidBody.drag = 0f;
-                    m_RigidBody.velocity = new Vector3(m_RigidBody.velocity.x, 0f, m_RigidBody.velocity.z);
-                    m_RigidBody.AddForce(new Vector3(0f, movementSettings.JumpForce, 0f), ForceMode.Impulse);
-                    m_Jumping = true;
-                }
-                
-                if (!m_Jumping && Mathf.Abs(input.x) < float.Epsilon && Mathf.Abs(input.y) < float.Epsilon && m_RigidBody.velocity.magnitude < 1f)
-                {
-                    m_RigidBody.Sleep();
-                }
-            }
-            else
-            {
-                m_RigidBody.drag = 0f;
-                if (m_PreviouslyGrounded && !m_Jumping)
-                {
-                    StickToGroundHelper();
-                }
-            }
-            m_Jump = false;
-            Collider hit = CheckGrab(cam.transform.forward).collider;
-            if (hit != null)
-            {
-                IInteractable food = hit.transform.GetComponent<IInteractable>();
-                if (Input.GetButtonDown("Hold" + playerNumber) && m_CanGrab)
-                {
-                    if (food != null)
+                    if (m_PreviouslyGrounded && !m_Jumping)
                     {
-                        food.Interact(this);
+                        StickToGroundHelper();
                     }
                 }
-                if (Input.GetButtonDown("Throw" + playerNumber))
+                m_Jump = false;
+                Collider hit = CheckGrab(cam.transform.forward).collider;
+                if (hit != null)
                 {
-                    if (food != null)
+                    IInteractable food = hit.transform.GetComponent<IInteractable>();
+                    if (Input.GetButtonDown("Hold" + playerNumber) && m_CanGrab)
                     {
-                        food.Action(this);
+                        if (food != null)
+                        {
+                            food.Interact(this);
+                        }
+                    }
+                    if (Input.GetButtonDown("Throw" + playerNumber))
+                    {
+                        if (food != null)
+                        {
+                            food.Action(this);
+                        }
                     }
                 }
             }
         }
 
+        private void OnTriggerEnter(Collider collision)
+        {
+            if (dead && collision.gameObject.tag == "Cat")
+            {
+                Debug.Log("Died");
+                cam.enabled = false;
+            }
+        }
 
         private float SlopeMultiplier()
         {
